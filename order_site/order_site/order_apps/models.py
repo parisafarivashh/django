fac59a6d7db4e881fc2b6148e03b45f9646d3b89
Parisa Farivash <p.farivash@fwutech.com>
2021-05-17 13:35:05 +0430

    create model customuser

--- order_site/order_site/order_apps/models.py
+++ order_site/order_site/order_apps/models.py
@@ -1,2 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, name, email, phone, password):
+        if not phone:
+            raise ValueError('the user must have a phone number')
+
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email, phone=phone)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, name, email, phone, password):
+        user = self.create_user(name, email, phone, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11, blank=False, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name
--- order_site/order_site/order_site/settings.py
+++ order_site/order_site/order_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'order_apps.apps.OrderAppsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'order_apps.CustomUser'
