2091f3891dbe2e5251ea85f99a15473503d59493
Parisa Farivash <p.farivash@fwutech.com>
2021-05-17 16:48:56 +0430

    create views

--- order_site/order_site/order_apps/views.py
+++ order_site/order_site/order_apps/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Product, Profile, CustomUser, Meson, Category
+from .serializers import CustomUserSerializer, ProductSerializer, ProfileSerializer, MesonSerializer, CategorySerializer
+from rest_framework import viewsets
+from rest_framework.viewsets import generics
 
-# Create your views here.
+
+class SignUp(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class MesonViewSet(viewsets.ModelViewSet):
+    queryset = Meson.objects.all()
+    serializer_class = MesonSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
