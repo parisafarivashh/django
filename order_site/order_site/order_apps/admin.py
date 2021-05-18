341ee36e7ff237249cf7719b383a78e856e581b2
Parisa Farivash <p.farivash@fwutech.com>
2021-05-18 09:18:05 +0430

    create admin for models

--- order_site/order_site/order_apps/admin.py
+++ order_site/order_site/order_apps/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Profile, Category, CustomUser, Meson
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(CustomUser)
+admin.site.register(Meson)
