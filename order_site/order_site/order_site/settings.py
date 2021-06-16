2d6d72c7baf6550c72d990f14020054358a0cc59
Parisa Farivash <p.farivash@fwutech.com>
2021-06-16 12:04:53 +0430

    add MEDIA_URL and MEDIA_ROOT to setting

--- order_site/order_site/order_site/settings.py
+++ order_site/order_site/order_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +130,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'order_apps.CustomUser'
+MEDIA_URL = '/pictures/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_pictures')
