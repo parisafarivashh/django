ca5f82a4c57fabedf5e88f26cc814ed324b4482b
Parisa Farivash <p.farivash@fwutech.com>
2021-06-21 15:00:51 +0430

    create custom class admin

--- order_site/order_site/order_apps/admin.py
+++ order_site/order_site/order_apps/admin.py
@@ -1,8 +1,42 @@
 from django.contrib import admin
-from .models import Product, Profile, Category, CustomUser, Meson
+from django.db.models import Count
+from .models import Product, Profile, Category, CustomUser, Meson, Order,ItemOrder
 
-admin.site.register(Profile)
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(CustomUser)
-admin.site.register(Meson)
+
+@admin.register(ItemOrder)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("product", "cost")
+
+    def cost(self, obj):
+        cost = obj.price * obj.count
+        return cost
+
+
+@admin.register(Meson)
+class MesonAdmin(admin.ModelAdmin):
+    list_display = ("name", "show_product")
+
+    def show_product(self, obj):
+        product = obj.products.values_list("name", flat=True)
+        print(list(product))
+        return product[0:]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "color")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", )
